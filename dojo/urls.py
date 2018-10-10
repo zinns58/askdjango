370e3bdb44f8ea5f5436c6daf35e18cd5839fecd
zinns58 <zinns58@gmail.com>
2018-10-10 23:57:53 +0900

    dojo.vies.mysum2에서 다수 숫자를 받도록 개선

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('sum/<int:x>/', views.mysum),
     path('sum/<int:x>/<int:y>/', views.mysum),
     path('sum/<int:x>/<int:y>/<int:z>/', views.mysum),
+    re_path(r'^sum2/(?P<numbers>[\d/]+)/$', views.mysum2),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def mysum(request, x, y=0, z=0):
     return HttpResponse(x + y + z)
+
+def mysum2(request, numbers):
+    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+    return HttpResponse(result)
