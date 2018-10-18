49d4eaf1c1ccb3374286933f7a9c44d78658aab4
zinns58 <zinns58@gmail.com>
2018-10-18 14:31:05 +0900

    blog.models.Post 모델 유효성 검사 추가

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
 # blog/models.py
+import re
 from django.db import models
+from django.forms import ValidationError
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목',
                              help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
     content = models.TextField(verbose_name='내용')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True,
+                              validators=[lnglat_validator],
+                              help_text='위도/경도 포맷으로 입력')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
