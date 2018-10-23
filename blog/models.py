4986915ba2a447a034e4e1bd42a2e19b1cc1d8ae
zinns58 <zinns58@gmail.com>
2018-10-23 14:54:14 +0900

    django-extensions 추가

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목',
                              help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
     content = models.TextField(verbose_name='내용')
