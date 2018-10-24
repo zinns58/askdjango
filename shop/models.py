7c0730de705b4d5e234e77ed26f36e2771b95eea
zinns58 <zinns58@gmail.com>
2018-10-25 01:32:23 +0900

    related_name 예시로서, shop.models.Post 모델 생성

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='shop_post_set')
+    mesaage = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
