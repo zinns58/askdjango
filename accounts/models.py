6174cd034c6fdc8a0f10d30d0453b77bf7605780
zinns58 <zinns58@gmail.com>
2018-10-25 01:07:15 +0900

    settings.AUTH_USER_MODEL 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.db import models
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # todo : BAD CASE
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)  # BAD CASE
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # GOOD CASE
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
