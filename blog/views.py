67249439381d7f847cb17519b23ad3954053ccab
zinns58 <zinns58@gmail.com>
2018-10-24 23:00:32 +0900

    blog에 포스팅 상세페이지 추가, 목록페이지에 상세페이지 링크 추가

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
@@ -12,4 +13,19 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
         'q': q,
+    })
+
+
+def post_detail(request, id):
+    # 404 Error 방법 1 : model.object.get 으로 접근하는 방법은 지향하는것이 좋음.
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404
+
+    # 404 Error 방법 2 : 추천
+    post = get_object_or_404(Post, id=id)
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
     })
