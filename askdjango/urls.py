81ae020a8d6c7a0b7f7fd65ced7e5541c6960853
zinns58 <zinns58@gmail.com>
2018-10-25 00:42:49 +0900

    accounts 앱을 생성/등록

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
     path('dojo/', include('dojo.urls')),
 ]
