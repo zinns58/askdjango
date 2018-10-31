dac0b86b571c4caae152adea010a510ec878577f
zinns58 <zinns58@gmail.com>
2018-10-31 18:22:43 +0900

    url reverse 샘플코드 추가

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list),
-    path('<id>', views.post_detail),
+    path('', views.post_list, name="post_list"),
+    path('<id>', views.post_detail, name="post_detail"),
 ]
