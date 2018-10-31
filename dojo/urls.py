ba60010e7b2240550db993812f5319899e2fde79
zinns58 <zinns58@gmail.com>
2018-10-31 19:04:37 +0900

    각 app 마다 app_name 적용(django 2.0 기준) : 장고 이전 버전의 url namespace 기능

--- dojo/urls.py
+++ dojo/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path
 from . import views
 from . import views_cbv
 
+app_name = 'dojo'
+
 urlpatterns = [
     path('sum/<int:x>/', views.mysum),
     path('sum/<int:x>/<int:y>/', views.mysum),
