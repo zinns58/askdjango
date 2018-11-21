b52f3b1fe43be2e9e48d7d45c203be8acca9534e
zinns58 <zinns58@gmail.com>
2018-11-21 19:30:20 +0900

    Form.is_valid() 체크후에 cleaned_data를 사용해야하는 이유

--- dojo/forms.py
+++ dojo/forms.py
@@ -21,3 +21,20 @@ class PostForm(forms.Form):
             self.instance.save()
         return self.instance
 '''
+
+
+'''
+Form에서 제공하는 2가지 유효성 검사
+1) validator 함수를 통한 유효성 검사
+ - 값이 원하는 조건에 맞지 않을때, ValidationError 예외를 발생
+ - 리턴값을 쓰지 않음
+2) Form 클래스내 clean 맴버함수를 통한 유효성 검사 및 값 변경
+ - 값이 원하는 조건에 맞지 않을때, ValidationError예외를 발생
+ - 리턴값을 통해 값 변경
+결론) 프론트앤드에서 입력받은 데이터를 검사하고, 값을 변경하여 사용하므로, views 내에서 끝까지 cleaned_data의 값을 사용해야함.
+
+예시)
+class CommentForm(forms.Form):
+    def clean_message(self):
+        return self.cleaned_data.get('message', '').strip() # 좌우 공백 제거
+'''
