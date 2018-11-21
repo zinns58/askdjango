40b87782379fa02728a4d06661100ae411a1208a
zinns58 <zinns58@gmail.com>
2018-11-21 17:05:20 +0900

    blog.views.post_new/post_edit 뷰 구현

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,10 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name="post_list"),
-    path('<id>', views.post_detail, name="post_detail"),
+    path('<int:id>', views.post_detail, name="post_detail"),
+
+    path('new', views.post_new, name="post_new"),
+    path('<int:id>/edit', views.post_edit, name="post_edit"),
 
     path('cbv/new/', views_cbv.post_new),
 ]
