5cd8b785e13f0bafff7a94fcfed55721c911027d
zinns58 <zinns58@gmail.com>
2018-12-06 17:31:13 +0900

    messages framework의 level/tags 설정 변경

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = '/static/'
 
 
 INTERNAL_IPS = ['127.0.0.1']
+
+
+from django.contrib.messages import constants
+
+MESSAGE_LEVEL = constants.DEBUG # 지금부터 debug 레벨의 message를 남길수 있음.
+MESSAGE_TAGS = {constants.ERROR: 'danger'}
