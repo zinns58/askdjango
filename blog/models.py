ea5037053d30049d65587a908129b7e45301fc9b
zinns58 <zinns58@gmail.com>
2018-12-07 12:13:25 +0900

    MEDIA 설정을 하고, blog.models.Post 모델을 photo 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목',
                              help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
     content = models.TextField(verbose_name='내용')
+    photo = models.ImageField(blank=True)
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
                               validators=[lnglat_validator],
