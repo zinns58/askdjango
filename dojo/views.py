a71d5bb2a2980560ac67f107f36f02c271e59008
zinns58 <zinns58@gmail.com>
2018-12-10 18:10:51 +0900

    CBV 샘플 - STEP 2. 함수를 통해 뷰 생성

--- dojo/views.py
+++ dojo/views.py
@@ -6,11 +6,27 @@ from .forms import PostForm
 from .models import Post
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id)
-    return render(request, 'dojo/post_detail.html', {
-        'post': post,
-    })
+# CBV 샘플 STEP 1
+# def post_detail(request, id):
+#     post = get_object_or_404(Post, id=id)
+#     return render(request, 'dojo/post_detail.html', {
+#         'post': post,
+#     })
+
+
+# CBV 샘플 STEP 2
+def generate_view_fn(model):
+    def view_fn(request, id):
+        instance = get_object_or_404(model, id=id)
+        instance_name = model._meta.model_name
+        template_name = '{}/{}_detail.html'.format(model._meta.app_label, instance_name)
+        return render(request, template_name, {
+            instance_name: instance,
+        })
+    return view_fn
+
+post_detail = generate_view_fn(Post)
+
 
 def post_new(request):
     if request.method == 'POST':
