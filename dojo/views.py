31763e81a07c980b23bb2dda2fcb51bdee69787c
zinns58 <zinns58@gmail.com>
2018-12-10 22:39:15 +0900

    CBV 샘플 - STEP 4. CBV 구현

--- dojo/views.py
+++ dojo/views.py
@@ -2,6 +2,7 @@ import os
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import DetailView
 from .forms import PostForm
 from .models import Post
 
@@ -28,29 +29,33 @@ from .models import Post
 # post_detail = generate_view_fn(Post)
 
 # CBV 샘플 STEP 3
-class DetailView(object):
-    def __init__(self, model):
-        self.model = model
+# class DetailView(object):
+#     def __init__(self, model):
+#         self.model = model
+#
+#     def get_object(self, *args, **kwargs):
+#         return get_object_or_404(self.model, id=kwargs['id'])
+#
+#     def get_template_name(self):
+#         return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         return render(request, self.get_template_name(), {
+#             self.model._meta.model_name: self.get_object(*args, **kwargs),
+#         })
+#
+#     @classmethod
+#     def as_view(cls, model):
+#         def view(request, *args, **kwargs):
+#             self = cls(model)
+#             return self.dispatch(request, *args, **kwargs)
+#         return view
+#
+# post_detail = DetailView.as_view(Post)
 
-    def get_object(self, *args, **kwargs):
-        return get_object_or_404(self.model, id=kwargs['id'])
+# CBV 샘플 STEP 4
+post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
-    def get_template_name(self):
-        return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
-
-    def dispatch(self, request, *args, **kwargs):
-        return render(request, self.get_template_name(), {
-            self.model._meta.model_name: self.get_object(*args, **kwargs),
-        })
-
-    @classmethod
-    def as_view(cls, model):
-        def view(request, *args, **kwargs):
-            self = cls(model)
-            return self.dispatch(request, *args, **kwargs)
-        return view
-
-post_detail = DetailView.as_view(Post)
 
 def post_new(request):
     if request.method == 'POST':
