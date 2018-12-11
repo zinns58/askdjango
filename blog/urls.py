2c9501acb8074a394ff705e3933234606cd9f233
zinns58 <zinns58@gmail.com>
2018-12-11 11:48:07 +0900

    CBV : DetailView 적용

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views_cbv.post_list, name="post_list"),
-    path('<int:id>', views.post_detail, name="post_detail"),
+    path('<int:pk>', views_cbv.post_detail, name="post_detail"),
 
     path('new', views.post_new, name="post_new"),
     path('<int:id>/edit', views.post_edit, name="post_edit"),
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,8 +1,11 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 from .models import Post
 
 
 post_list = ListView.as_view(model=Post, paginate_by=3)
+
+
+post_detail = DetailView.as_view(model=Post)
 
 
 class PostCreateView(CreateView):
