7a81b6bde59817fe6fc5c125b0577b7f5ca293ba
zinns58 <zinns58@gmail.com>
2018-12-12 14:42:06 +0900

    로그인/로그아웃 구현 및 링크에 next인자 지정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
-    path('profile/', views.profile),
+    path('login/', auth_views.LoginView, name='login'),
+    path('logout/', auth_views.LogoutView, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
