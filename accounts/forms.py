7b7d65e0e442f0873d14172cbb9b4902364c28f0
zinns58 <zinns58@gmail.com>
2018-12-13 15:42:38 +0900

    로그인/회원가입 django2.0에 맞게 수정 및 퀴즈를 맞춰야 로그인 허용

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import Profile
 
 
@@ -18,3 +18,12 @@ class SignupForm(UserCreationForm):
             address = self.cleaned_data['address']
         )
         return user
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(label='3+3=?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer', None)
+        if answer != 6:
+            raise forms.ValidationError('mismatched!')
+        return answer
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,19 @@
 from django.conf import settings
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView, name='login'),
-    path('logout/', auth_views.LogoutView, name='logout'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='accounts/login_form.html',
+        authentication_form=LoginForm,
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page=settings.LOGIN_URL
+    ), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
